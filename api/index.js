f98fce7b569c1d26d39b96adc39c985a68a3791e
brellin <cekakid@gmail.com>
2019-05-07 13:53:38 -0500

    added session and cookies

--- api/index.js
+++ api/index.js
@@ -2,9 +2,23 @@ const express = require('express')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const auth = require('./auth')
+const session = require('express-session');
 
 const server = express()
 
+const sessionConfig = {
+    name: 'webauth-1',
+    secret: "here's the secret.",
+    cookie: {
+        httpOnly: true,
+        maxAge: 60000 * 60,
+        secure: false
+    },
+    resave: false,
+    saveUninitialized: true
+}
+
+server.use(session(sessionConfig))
 server.use(express.json())
 server.use(morgan('dev'))
 server.use(helmet())
